62515cd6aeb6f5fead52302abb761574c6b97c37
James Brown <jamesbbb1@gmail.com>
2019-11-28 14:24:59 -0600

    adding assignment1.1.js

---
+++ assignment1.1.js
@@ -0,0 +1,44 @@
+/*
+============================================
+; Title: Assignment 1.1
+; Author: James Brown
+; Date: 28 November 2019
+; Modified By: James Brown
+; Description:  This is a simple JavaScript 
+program using values and variables with at least two errors.
+
+The intended functionality is to set a variable to true, 
+then check to see if it is actually true. If the variable is set to true,
+the if statement should log "The variable is actually true to the console.
+If the variable is set to false, it will log the variable is actually false
+to the console"
+===========================================
+*/ 
+
+//Start Program
+
+/**
+ * Expected output
+ * The variable isTrue is actually True
+ */
+
+
+ 
+
+//variable declaration and assignment goes here
+var isTrue = True;
+
+
+
+//output
+if (isTrue){
+    console.lag(`The variable ${isTrue} is actually True`);
+}else{
+    console.log(`The variable ${isTrue} is actually false`);
+}
+
+
+
+//End program
+
+
