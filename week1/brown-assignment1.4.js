86905da5242625060366f93dcec2ac13a0b1e74c
James Brown <jamesbbb1@gmail.com>
2019-11-29 10:05:51 -0600

    adding 1.3 and 1.4 assignments

---
+++ week1/brown-assignment1.4.js
@@ -0,0 +1,26 @@
+/* 
+Title: Assignment 1.4 - Primitive types; 
+Author: James Brown; 
+Date: 29 November 2019; 
+Description: Completed code per instructions in 
+blackboard.;
+*/
+
+
+var firstName = `James`;
+var lastName = `Brown`;
+var age = 27;
+var street = `123 Test st`;
+var city = `Omaha`;
+var zipCode = `68109`;
+
+
+console.log(`
+             ${firstName}  
+             ${lastName} 
+             ${age} 
+             ${street} 
+             ${city} 
+             ${zipCode}
+             `);
+             
--- week1/hello-world.js
+++ week1/hello-world.js
@@ -1,1 +1,11 @@
+/* 
+Title: Assignment 1.3 - Hello-World; 
+Author: James Brown; 
+Date: 29 November 2019; 
+Description: Completed code per instructions in 
+blackboard.;
+*/
+
+
+
 console.log(`Hello World! My name is James Brown`);
